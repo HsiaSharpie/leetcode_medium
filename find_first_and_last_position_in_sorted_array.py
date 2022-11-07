25be3caf35c476f81df6c5260f206aab9ecbdeae
samhsia <samhsia@cruxover.com>
2022-11-07 21:05:48 +0800

    upload find_first_and_last_position_in_sorted_array

---
+++ find_first_and_last_position_in_sorted_array.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binSearch(nums, target, leftBias=True)
+        right = self.binSearch(nums, target, leftBias=False)
+        return [left, right]
+
+    def binSearch(self, nums, target, leftBias):
+        left, right = 0, len(nums) - 1
+        i = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                i = mid
+                if leftBias:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return i
